5ff3f5e5cad777b0501efe1d19446369bf61d0f0
Juan Manuel Zaragoza <jzaragoza@fi.uba.ar>
2021-05-04 15:56:09 -0300

    Hardcoded dashboard

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { printLine } from './modules/print';
-import DomButton from '../../components/DomButton';
 import {insertChild} from './modules/dom';
+import FullDashboard from '../../containers/FullDashboard';
 
 console.log('IVEE Content script works!');
 console.log('Must reload extension for modifications to take effect.');
@@ -12,4 +12,4 @@ printLine("Using the 'printLine' function from the Print Module");
 const element = document.getElementsByClassName('title')[0];
 const app = document.createElement('span');
 insertChild(element, app);
-ReactDOM.render(<DomButton />, app);
+ReactDOM.render(<FullDashboard />, app);
